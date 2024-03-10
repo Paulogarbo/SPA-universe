ff96d42037ba3243d02f8f41b5b46c956efcbc56
Paulo Alex <108588057+Paulogarbo@users.noreply.github.com>
2024-03-10 08:34:22 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import "./events.js"
 import * as el from './elements.js'
 import { changeBackground } from "./change-back.js"
 
-el.router.add('/SPA-universe/', './pages/home.html')
-el.router.add('/SPA-universe/universe', './pages/the_universe.html')
-el.router.add('/SPA-universe/explore', './pages/explorer.html')
+el.router.add('/', './pages/home.html')
+el.router.add('/universe', './pages/the_universe.html')
+el.router.add('/explore', './pages/explorer.html')
 el.router.add('404', './pages/404.html')
 
 
